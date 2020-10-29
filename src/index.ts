7e3af796f0715abfd5e442239ea7c25ef380495f
Gabor <gabor.gyorgy@and.digital>
2020-10-29 16:07:12 +0000

    Added updated joi middlewares to the index file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
 import express, { Application, Request, Response } from "express";
-import { JoiValidationExample } from "./middlewares";
+import bodyParser from "body-parser";
+import { JoiValidationExample, JoiValidationExample2 } from "./middlewares";
+import {
+  validateCreateTransaction,
+  CreateTransactionSchema,
+} from "./validationSchemas/joi";
 
 const app: Application = express();
 const port: number = 8080;
 
-app.post("/", JoiValidationExample, (_req: Request, res: Response) => {
-  res.json({ hello: "world" });
+app.use(bodyParser.json());
+
+app.post(
+  "/joi-example-1",
+  JoiValidationExample(validateCreateTransaction),
+  (_req: Request, res: Response) => {
+    res.json({ hello: "world" });
+  }
+);
+
+app.post(
+  "/joi-example-2",
+  JoiValidationExample2(CreateTransactionSchema),
+  (_req: Request, res: Response) => {
+    res.json({ hello: "world" });
+  }
+);
+
+app.use((err, _req: Request, res: Response) => {
+  if (err) {
+    return res.status(500).json({
+      success: false,
+      error: err,
+    });
+  }
 });
 
 app.listen(port, () => {
